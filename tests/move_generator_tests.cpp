28997fc60f6ee484e72b0de8ef311c3a635ccc31
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2023-07-11 02:03:46 +0100

    Fix move generator?

--- tests/move_generator_tests.cpp
+++ tests/move_generator_tests.cpp
@@ -11,8 +11,13 @@ TEST(MoveGeneratorTests, CheckCorrectMoveCountStartPos) {
     ASSERT_EQ(lst.len(), 20);
     ASSERT_EQ(MoveGenerator::generate_pawn_moves(c, 0).len(), 16);
     ASSERT_EQ(MoveGenerator::generate_knight_moves(c, 0).len(), 4);
+
+    lst = MoveGenerator::generate_moves(c, 1);
+    ASSERT_EQ(lst.len(), 20);
+    ASSERT_EQ(MoveGenerator::generate_pawn_moves(c, 1).len(), 16);
+    ASSERT_EQ(MoveGenerator::generate_knight_moves(c, 1).len(), 4);
 }
-#include "../src/magic_numbers.hpp"
+
 TEST(MoveGeneratorTests, CheckCorrectMoveCountMaxMoves) {
     ChessBoard c;
     c.set_from_fen("R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1 ");
@@ -20,6 +25,5 @@ TEST(MoveGeneratorTests, CheckCorrectMoveCountMaxMoves) {
     ASSERT_EQ(rook_moves.len(), 19);
 
     auto lst = MoveGenerator::generate_moves(c, 0);
-    print_bitboard(MoveGenerator::generate_rook_movemask(c, 8));
     ASSERT_EQ(lst.len(), 218);
 }
