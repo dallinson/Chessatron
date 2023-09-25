a37bbfcfd68d8b16a076bcc0103d86dfcbc9188f
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2023-09-26 00:09:08 +0100

    Mark chessboard functions as inline

--- src/chessboard.hpp
+++ src/chessboard.hpp
@@ -36,32 +36,32 @@ class ChessBoard {
         inline Bitboard get_pair_occupancy(int offset) const { return bitboards[offset] | bitboards[offset + 1]; };
 
     public:
-        Bitboard get_occupancy() const {
+        inline Bitboard get_occupancy() const {
             return get_king_occupancy() | get_queen_occupancy() | get_bishop_occupancy() | get_knight_occupancy() | get_rook_occupancy() |
                    get_pawn_occupancy();
         };
-        Bitboard get_occupancy(const int side) const { return get_side_occupancy(side); };
+        inline Bitboard get_occupancy(const int side) const { return get_side_occupancy(side); };
 
-        Bitboard get_side_occupancy(const int side) const {
+        inline Bitboard get_side_occupancy(const int side) const {
             return bitboards[KING_OFFSET + side] | bitboards[QUEEN_OFFSET + side] | bitboards[BISHOP_OFFSET + side] |
                    bitboards[KNIGHT_OFFSET + side] | bitboards[ROOK_OFFSET + side] | bitboards[PAWN_OFFSET + side];
         };
-        Bitboard get_white_occupancy() const { return get_side_occupancy(WHITE_IDX); };
-        Bitboard get_black_occupancy() const { return get_side_occupancy(BLACK_IDX); };
+        inline Bitboard get_white_occupancy() const { return get_side_occupancy(WHITE_IDX); };
+        inline Bitboard get_black_occupancy() const { return get_side_occupancy(BLACK_IDX); };
 
-        Bitboard get_king_occupancy() const { return get_pair_occupancy(KING_OFFSET); };
-        Bitboard get_queen_occupancy() const { return get_pair_occupancy(QUEEN_OFFSET); };
-        Bitboard get_bishop_occupancy() const { return get_pair_occupancy(BISHOP_OFFSET); };
-        Bitboard get_knight_occupancy() const { return get_pair_occupancy(KNIGHT_OFFSET); };
-        Bitboard get_rook_occupancy() const { return get_pair_occupancy(ROOK_OFFSET); };
-        Bitboard get_pawn_occupancy() const { return get_pair_occupancy(PAWN_OFFSET); };
+        inline Bitboard get_king_occupancy() const { return get_pair_occupancy(KING_OFFSET); };
+        inline Bitboard get_queen_occupancy() const { return get_pair_occupancy(QUEEN_OFFSET); };
+        inline Bitboard get_bishop_occupancy() const { return get_pair_occupancy(BISHOP_OFFSET); };
+        inline Bitboard get_knight_occupancy() const { return get_pair_occupancy(KNIGHT_OFFSET); };
+        inline Bitboard get_rook_occupancy() const { return get_pair_occupancy(ROOK_OFFSET); };
+        inline Bitboard get_pawn_occupancy() const { return get_pair_occupancy(PAWN_OFFSET); };
 
-        Bitboard get_king_occupancy(const int side) const { return bitboards[KING_OFFSET + side]; };
-        Bitboard get_queen_occupancy(const int side) const { return bitboards[QUEEN_OFFSET + side]; };
-        Bitboard get_bishop_occupancy(const int side) const { return bitboards[BISHOP_OFFSET + side]; };
-        Bitboard get_knight_occupancy(const int side) const { return bitboards[KNIGHT_OFFSET + side]; };
-        Bitboard get_rook_occupancy(const int side) const { return bitboards[ROOK_OFFSET + side]; };
-        Bitboard get_pawn_occupancy(const int side) const { return bitboards[PAWN_OFFSET + side]; };
+        inline Bitboard get_king_occupancy(const int side) const { return bitboards[KING_OFFSET + side]; };
+        inline Bitboard get_queen_occupancy(const int side) const { return bitboards[QUEEN_OFFSET + side]; };
+        inline Bitboard get_bishop_occupancy(const int side) const { return bitboards[BISHOP_OFFSET + side]; };
+        inline Bitboard get_knight_occupancy(const int side) const { return bitboards[KNIGHT_OFFSET + side]; };
+        inline Bitboard get_rook_occupancy(const int side) const { return bitboards[ROOK_OFFSET + side]; };
+        inline Bitboard get_pawn_occupancy(const int side) const { return bitboards[PAWN_OFFSET + side]; };
 
         /**
          * @brief Get the en passant file
@@ -71,10 +71,10 @@ class ChessBoard {
         uint_fast8_t get_en_passant_file() const { return en_passant_file; };
         void set_en_passant_file(int file) { en_passant_file = file; };
 
-        bool get_queenside_castling(const int side) const { return queenside_castling[side]; };
-        bool get_kingside_castling(const int side) const { return kingside_castling[side]; };
-        void set_kingside_castling(const int side, const bool val) { kingside_castling[side] = val; };
-        void set_queenside_castling(const int side, const bool val) { queenside_castling[side] = val; };
+        inline bool get_queenside_castling(const int side) const { return queenside_castling[side]; };
+        inline bool get_kingside_castling(const int side) const { return kingside_castling[side]; };
+        inline void set_kingside_castling(const int side, const bool val) { kingside_castling[side] = val; };
+        inline void set_queenside_castling(const int side, const bool val) { queenside_castling[side] = val; };
 
         int get_score(int side);
 
