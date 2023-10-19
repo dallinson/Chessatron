59e24e23682095b07c7bfd3c5553dfe65d10e9b4
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2023-10-19 01:27:50 +0100

    Rewrite iterative deepening and get board score in centipawns

--- src/search.cpp
+++ src/search.cpp
@@ -123,18 +123,13 @@ int32_t SearchHandler::quiescent_search(int32_t alpha, int32_t beta, std::unorde
     }
 
     auto result = had_move ? alpha : c.evaluate();
-    if (!transpositions.contains(c)) {
-        transpositions[c] = std::pair(0, result);
-    }
     return result;
 }
 
-Move SearchHandler::run_negamax(int depth, std::unordered_map<ChessBoard, std::pair<int, int32_t>>& transpositions) {
+/*Move SearchHandler::run_negamax(int depth, std::unordered_map<ChessBoard, std::pair<int, int32_t>>& transpositions) {
     auto moves = MoveGenerator::generate_pseudolegal_moves(c, c.get_side_to_move());
     Move best_move = 0;
     int best_score = MagicNumbers::NegativeInfinity;
-    int32_t alpha = MagicNumbers::NegativeInfinity;
-    int32_t beta = MagicNumbers::PositiveInfinity;
     for (size_t i = 0; i < moves.len(); i++) {
         const auto& move = moves[i];
         if (!MoveGenerator::is_move_legal(c, move)) {
@@ -155,17 +150,62 @@ Move SearchHandler::run_negamax(int depth, std::unordered_map<ChessBoard, std::p
         }
     }
     return best_move;
-}
+}*/
 
 Move SearchHandler::run_iterative_deepening_search() {
-    Move best_move = 0;
+    Move best_move_so_far = 0;
     std::unordered_map<ChessBoard, std::pair<int, int32_t>> transpositions;
-    for (int i = 1; i < perft_depth && !search_cancelled; i++) {
-        auto result = run_negamax(i, transpositions);
+    const auto moves = MoveGenerator::generate_legal_moves(c, c.get_side_to_move());
+    // We generate legal moves only as it saves us having to continually rerun legality checks
+    for (int depth = 1; depth < perft_depth && !search_cancelled; depth++) {
+        int best_score_this_depth = MagicNumbers::NegativeInfinity;
+        Move best_move_this_depth;
+        // We need to init for the depth=1 iteration where no PV-move exists
+        int32_t alpha = MagicNumbers::NegativeInfinity;
+        int32_t beta = MagicNumbers::PositiveInfinity;
+        int32_t score = MagicNumbers::NegativeInfinity;
+        if (!best_move_so_far.is_null_move()) {
+            c.make_move(best_move_so_far, m);
+            score = -negamax_step(-beta, -alpha, depth - 1, transpositions);
+            c.unmake_move(m);
+            alpha = std::max(score, alpha);
+            best_score_this_depth = score;
+            best_move_this_depth = best_move_so_far;
+        }
+
+        if (score < beta) {
+            // If score >= beta then we beta-prune every other move!
+            for (size_t i = 0; i < moves.len(); i++) {
+                if (moves[i] == best_move_so_far) {
+                    // If it has already been examined
+                    continue;
+                }
+                c.make_move(moves[i], m);
+                score = -negamax_step(-beta, -alpha, depth - 1, transpositions);
+                c.unmake_move(m);
+                alpha = std::max(score, alpha);
+                if (score > best_score_this_depth) {
+                    best_score_this_depth = score;
+                    best_move_this_depth = moves[i];
+                }
+            }
+        }
+
         if (!search_cancelled) {
-            printf("info depth %d bestmove %s\n", i, result.to_string().c_str());
-            best_move = result;
+            printf("info depth %d bestmove %s ", depth, best_move_this_depth.to_string().c_str());
+            if (std::abs(best_score_this_depth) == MagicNumbers::PositiveInfinity) {
+                // If this is a checkmate
+                printf("mate %d\n", ((best_score_this_depth / std::abs(best_score_this_depth)) * depth) / 2);
+                // Divide by 2 as mate expects the result in moves, not plies
+                if (best_score_this_depth == MagicNumbers::PositiveInfinity) {
+                    return best_move_this_depth;
+                }
+                // No need to check any other moves if we can guarantee a mate
+            } else {
+                printf("cp %d\n", best_score_this_depth);
+            }
+            best_move_so_far = best_move_this_depth;
         }
     }
-    return best_move;
+    return best_move_so_far;
 }
