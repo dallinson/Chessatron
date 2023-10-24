11c1953328e9f370627154ac816c7472e0d443dd
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2023-10-25 00:15:37 +0100

    Modify PositiveInfinity value

--- src/magic_numbers.hpp
+++ src/magic_numbers.hpp
@@ -28,7 +28,10 @@ namespace MagicNumbers {
     extern const Bitboard KnightMoves[64];
     extern const Bitboard PawnAttacks[128];
 
-    const int32_t PositiveInfinity = std::numeric_limits<int32_t>::max();
+    const int32_t PositiveInfinity = 1073741823;
+    // This is (2^30)-1; int32_t::max is (2^31)-1 so this affords some leeway for maths to be performed
+    // If PositiveInfinity was (2^31)-1 then segfaults could occur as overflow means mates were missed
+    // and checking for checks happens without a king
     const int32_t NegativeInfinity = -PositiveInfinity;
-    // int32_t so a known constant size and max as in a 32-bit int abs(highest value) is 1 lower than abs(lowest_value)
+    // int32_t so a known constant size
 } // namespace MagicNumbers
