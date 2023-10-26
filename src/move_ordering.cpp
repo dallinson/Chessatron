e3c6bdb1231921ed08868d8dad941a75109abc09
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2023-10-26 12:08:06 +0100

    Implement MVV-LVA heuristic for move ordering

--- src/move_ordering.cpp
+++ src/move_ordering.cpp
@@ -14,9 +14,11 @@ size_t MoveOrdering::reorder_captures(MoveList& move_list, const ChessBoard& c) 
         }
     }
     std::sort(&move_list[0], &move_list[captures], [c](const Move a, const Move b) {
-        int a_score = Evaluation::get_piece_score(c.get_piece(a.get_dest_square()).get_type());
-        int b_score = Evaluation::get_piece_score(c.get_piece(b.get_dest_square()).get_type());
-        return a_score > b_score;
+        int a_target = Evaluation::get_piece_score(c.get_piece(a.get_dest_square()).get_type());
+        int a_source = Evaluation::get_piece_score(c.get_piece(a.get_src_square()).get_type());
+        int b_target = Evaluation::get_piece_score(c.get_piece(b.get_dest_square()).get_type());
+        int b_source = Evaluation::get_piece_score(c.get_piece(b.get_src_square()).get_type());
+        return (a_target - a_source) > (b_target - b_source);
         // Reverse order to ensure that moves are in ascending order
     });
     return captures;
