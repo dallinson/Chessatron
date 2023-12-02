af10451b4a394a13d02aa6026e7711006d03c6a6
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2023-12-02 04:17:43 +0000

    Try reordering captures based on SEE
    Bench: 2343792

--- src/move_ordering.cpp
+++ src/move_ordering.cpp
@@ -19,7 +19,11 @@ void MoveOrdering::reorder_moves(MoveList& moves, const ChessBoard& board, const
             continue;
         }
         if (moves[i].move.is_capture()) {
-            moves[i].score += 1000000;
+            if (Search::static_exchange_evaluation(board, moves[i].move, 0)) {
+                moves[i].score += 1000000;
+            } else {
+                moves[i].score += 100000;
+            }
 
             const auto src_score = ordering_scores[static_cast<uint8_t>(board.get_piece(moves[i].move.get_src_square()).get_type()) - 1];
             const auto dest_type = moves[i].move.get_move_flags() == MoveFlags::EN_PASSANT_CAPTURE ? PieceTypes::PAWN : board.get_piece(moves[i].move.get_dest_square()).get_type();
