478e9302f41ab31439e12fca5a9fb685c7d3c40a
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2023-12-05 22:47:34 +0000

    Merge in history ordering
    Bench: 4990672

--- src/move_ordering.cpp
+++ src/move_ordering.cpp
@@ -12,12 +12,6 @@ constexpr std::array<uint8_t, 6> ordering_scores = { 1, 3, 3, 5, 9, 20 };
 void MoveOrdering::reorder_moves(MoveList& moves, const ChessBoard& board, const Move pv_move, bool& found_pv_move, std::array<uint32_t, 8192>& history_table) {
     found_pv_move = false;
     for (size_t i = 0; i < moves.len(); i++) {
-        if (board.get_piece(moves[i].move.get_src_square()).get_type() == PieceTypes::PAWN) {
-            std::reverse(&moves[i], &moves[moves.len()]);
-            break;
-        }
-    }
-    for (size_t i = 0; i < moves.len(); i++) {
         moves[i].score = 0;
         if (moves[i].move == pv_move) {
             found_pv_move = true;
