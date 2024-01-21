79e12bd8f22b7084dd5c97f0f3597ff11733f492
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2024-01-21 03:02:32 +0000

    Avoid using std::variant for time control info
    Bench: 111112219

--- src/time_management.hpp
+++ src/time_management.hpp
@@ -14,7 +14,12 @@ struct DepthTC;
 // Used for infinite time control
 struct InfiniteTC {};
 
-using TimeControlInfo = std::variant<FixedTimeTC, VariableTimeTC, DepthTC, InfiniteTC>;
+enum TCType {
+    FIXED_TIME_TC,
+    VARIABLE_TIME_TC,
+    DEPTH_TC,
+    INFINITE_TC,
+};
 
 struct FixedTimeTC {
     uint32_t search_time;
@@ -30,6 +35,26 @@ struct DepthTC {
     uint16_t depth;
 };
 
+struct TimeControlInfo {
+    TCType time_control_type;
+    union {
+        FixedTimeTC fixed_time_tc;
+        VariableTimeTC variable_time_tc;
+        DepthTC depth_tc;
+        InfiniteTC infinite_tc;
+    };
+
+    constexpr TimeControlInfo();
+    constexpr TimeControlInfo(FixedTimeTC fixed_tc) : time_control_type(TCType::FIXED_TIME_TC), fixed_time_tc(fixed_tc) {};
+    constexpr TimeControlInfo(VariableTimeTC variable_tc) : time_control_type(TCType::VARIABLE_TIME_TC), variable_time_tc(variable_tc) {};
+    constexpr TimeControlInfo(DepthTC depth_tc) : time_control_type(TCType::DEPTH_TC), depth_tc(depth_tc) {};
+    constexpr TimeControlInfo(InfiniteTC infinite_tc) : time_control_type(TCType::INFINITE_TC), infinite_tc(infinite_tc) {};
+};
+
+constexpr inline TimeControlInfo::TimeControlInfo() {
+    *this = TimeControlInfo(InfiniteTC {});
+}
+
 namespace TimeManagement {
     /**
      * @brief Determines if this std::variant is an instance of InfiniteTC
@@ -39,9 +64,7 @@ namespace TimeManagement {
      * @return false 
      */
     inline bool is_infinite_tc(const TimeControlInfo& tc) {
-        return std::visit([](const auto& tc) {
-            return std::is_same_v<std::decay_t<decltype(tc)>, InfiniteTC>;
-        }, tc);
+        return tc.time_control_type == TCType::INFINITE_TC;
     }
 
     /**
@@ -52,9 +75,7 @@ namespace TimeManagement {
      * @return false 
      */
     inline bool is_time_based_tc(const TimeControlInfo& tc) {
-        return std::visit([](const auto& tc) {
-            return std::is_same_v<std::decay_t<decltype(tc)>, FixedTimeTC> || std::is_same_v<std::decay_t<decltype(tc)>, VariableTimeTC>;
-        }, tc);
+        return tc.time_control_type == TCType::FIXED_TIME_TC || tc.time_control_type == TCType::VARIABLE_TIME_TC;
     }
 
     /**
@@ -64,13 +85,7 @@ namespace TimeManagement {
      * @return uint16_t The search depth if this std::variant is a DepthTC object, otherwise the maximum uint16_t value
      */
     inline uint16_t get_search_depth(const TimeControlInfo& tc) {
-        return std::visit([](const auto& tc) {
-            if constexpr (std::is_same_v<std::decay_t<decltype(tc)>, DepthTC>) {
-                return tc.depth;
-            } else {
-                return static_cast<uint16_t>(250);
-            }
-        }, tc);
+        return (tc.time_control_type == TCType::DEPTH_TC) ? tc.depth_tc.depth : 250;
     }
 
     /**
@@ -80,15 +95,14 @@ namespace TimeManagement {
      * @return uint32_t The search time if this is a time-based TC, otherwise the maximum uint32_t value
      */
     inline uint32_t get_search_time(const TimeControlInfo& tc) {
-        return std::visit([](const auto& tc) {
-            if constexpr (std::is_same_v<std::decay_t<decltype(tc)>, FixedTimeTC>) {
-                return tc.search_time;
-            } else if constexpr (std::is_same_v<std::decay_t<decltype(tc)>, VariableTimeTC>) {
-                return tc.search_time;
-            } else {
+        switch (tc.time_control_type) {
+            case TCType::FIXED_TIME_TC:
+                return tc.fixed_time_tc.search_time;
+            case TCType::VARIABLE_TIME_TC:
+                return tc.variable_time_tc.search_time;
+            default:
                 return std::numeric_limits<uint32_t>::max();
-            }
-        }, tc);
+        }
     }
 
     /**
