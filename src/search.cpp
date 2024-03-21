4acbb3b3399e389abfc2b3ea42d96b7bc06115af
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2024-03-21 12:34:27 +0000

    Remove max in lmr calculation
    Bench: 6009883

--- src/search.cpp
+++ src/search.cpp
@@ -351,11 +351,10 @@ Score SearchHandler::negamax_step(const ChessBoard& old_board, Score alpha, Scor
             && evaluated_moves >= std::max((size_t) 1, static_cast<size_t>(is_pv_node(node_type)) + static_cast<size_t>(!tt_move)
                                                            + static_cast<size_t>(node_type == NodeTypes::ROOT_NODE)
                                                            + static_cast<size_t>(move.move.is_capture() || move.move.is_promotion()))) {
-            const auto lmr_reduction = std::max(0, 
-                static_cast<int>(std::round(1.30 + ((MagicNumbers::LnValues[depth] * MagicNumbers::LnValues[evaluated_moves]) / 2.80)))
+            const auto lmr_reduction = static_cast<int>(std::round(1.30 + ((MagicNumbers::LnValues[depth] * MagicNumbers::LnValues[evaluated_moves]) / 2.80)))
                 + static_cast<int>(!is_pv_node(node_type) && is_cut_node)
                 - static_cast<int>(board.in_check()) // Reduce less if the board is in check
-                + static_cast<int>(!improving));
+                + static_cast<int>(!improving);
             score = -negamax_step<NodeTypes::NON_PV_NODE>(board, -(alpha + 1), -alpha, depth - lmr_reduction + extensions, ply + 1, node_count,
                                                           child_cutnode_type);
 
