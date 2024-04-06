42f64c9bf09c2185f382db639f544736e0fd4908
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2024-04-06 01:03:54 +0100

    Movepicker (#74)
    * Use staged selection sort
    
    Bench: 9237591
    
    * Use stable selection sort and update google test version
    
    Bench: 7606254
    
    * Change ordering sort to unstable incremental selection sort
    
    Bench: 8354186
    
    * Update github actions
    
    * Add clang setup step to github actions
    
    * Add clang install from apt to gh actions
    
    * Make clang apt install non-version-specific
    
    * Use official clang-17 install script
    
    * Do not check code coverage in gh actions
    
    * Remove deliberately failing test

--- src/move_ordering.hpp
+++ src/move_ordering.hpp
@@ -3,6 +3,14 @@
 #include "chessboard.hpp"
 #include "move.hpp"
 
-namespace MoveOrdering {
-    void reorder_moves(MoveList& moves, const ChessBoard& board, const Move pv_move, std::array<int32_t, 8192>& history_table, Move killer_move, bool& found_pv_move);
-} // namespace MoveOrdering
+class MovePicker {
+    private:
+        MoveList moves;
+        size_t idx;
+
+    public:
+        MovePicker(MoveList&& input_moves, const ChessBoard& board, const Move pv_move, std::array<int32_t, 8192>& history_table, Move killer, bool& found_pv_move);
+        std::optional<ScoredMove> next();
+
+        const ScoredMove& operator[](size_t idx) { return moves[idx]; };
+};
