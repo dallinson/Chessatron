48a3391716977754a840d11cd1f842f75b2c3e7e
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2024-05-28 15:50:55 +0100

    Clamp LMR-reduced depth between 1 and the remaining ply (#90)
    Bench: 7366929

--- src/search.cpp
+++ src/search.cpp
@@ -346,7 +346,7 @@ Score SearchHandler::negamax_step(const ChessBoard& old_board, Score alpha, Scor
             && total_moves >= std::max((size_t) 2, 1 + static_cast<size_t>(is_pv_node(node_type)) + static_cast<size_t>(!tt_move)
                                                            + static_cast<size_t>(node_type == NodeTypes::ROOT_NODE)
                                                            + static_cast<size_t>(move.move.is_capture() || move.move.is_promotion()))) {
-            const auto lmr_reduction = [&]() {
+            const auto lmr_depth = std::clamp(new_depth - [&]() {
                 int lmr_reduction = static_cast<int>(LmrTable[depth][total_moves - 1]);
                 // default log formula for lmr
                 lmr_reduction += static_cast<int>(!is_pv_node(node_type) && is_cut_node);
@@ -354,9 +354,9 @@ Score SearchHandler::negamax_step(const ChessBoard& old_board, Score alpha, Scor
                 lmr_reduction -= static_cast<int>(board.in_check());
                 // reduce less if we're in check
                 return lmr_reduction;
-            }();
+            }(), 1, MAX_PLY - ply);
             
-            score = -negamax_step<NodeTypes::NON_PV_NODE>(board, -(alpha + 1), -alpha, new_depth - lmr_reduction, ply + 1, node_count,
+            score = -negamax_step<NodeTypes::NON_PV_NODE>(board, -(alpha + 1), -alpha, lmr_depth, ply + 1, node_count,
                                                           child_cutnode_type);
 
             // it's possible the LMR score will raise alpha; in this case we re-search with the full depth
