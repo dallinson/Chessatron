e7e7d789214c5883e4535cd337e3a85e772c026e
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2024-06-17 00:52:21 +0100

    Add capthist
    Bench: 4193771

--- src/history.cpp
+++ src/history.cpp
@@ -3,8 +3,8 @@
 #include <algorithm>
 
 HistoryValue HistoryTable::score(const BoardHistory& hist, Move move, Side stm) const {
-    if (!move.is_quiet()) {
-        return 0;
+    if (move.is_capture()) {
+        return capthist_score(hist, move);
     } else {
         return mainhist_score(move, stm) + 2 * conthist_score(hist, move);
     }
@@ -16,11 +16,18 @@ void HistoryTable::update_scores(const BoardHistory& hist, std::span<const Move>
     if (!current_move.move.is_capture()) {
         update_mainhist_score(current_move.move, stm, hist_bonus);
         update_conthist_score(hist, current_move.move, hist_bonus);
+        std::for_each(moves.begin(), moves.end(), [&](Move move) {
+            if (!move.is_capture()) {
+                update_mainhist_score(move, stm, hist_malus);
+                update_conthist_score(hist, move, hist_malus);
+            }
+        });
+    } else {
+        update_capthist_score(hist, current_move.move, hist_bonus);
     }
     std::for_each(moves.begin(), moves.end(), [&](Move move) {
-        if (move.is_quiet()) {
-            update_mainhist_score(move, stm, hist_malus);
-            update_conthist_score(hist, move, hist_malus);
+        if (move.is_capture() && move != current_move.move) {
+            update_capthist_score(hist, move, hist_malus);
         }
     });
 }
@@ -43,4 +50,22 @@ HistoryValue HistoryTable::conthist_score(const BoardHistory& hist, Move move) c
     } else {
         return 0;
     }
+}
+
+
+HistoryValue HistoryTable::capthist_score(const BoardHistory& hist, const Move move) const {
+    const auto& pos = hist[hist.len() - 1];
+    const auto captured_type = (move.is_promotion() || move.get_move_flags() == MoveFlags::EN_PASSANT_CAPTURE)
+        ? PieceTypes::PAWN
+        : pos.piece_at(move.dst_sq()).get_type();
+    return (*capt_hist)[pos.piece_to(move)][static_cast<int>(captured_type) - 1];
+}
+
+void HistoryTable::update_capthist_score(const BoardHistory& hist, Move move, HistoryValue bonus) {
+    const auto& pos = hist[hist.len() - 1];
+    const auto captured_type = (move.is_promotion() || move.get_move_flags() == MoveFlags::EN_PASSANT_CAPTURE)
+        ? PieceTypes::PAWN
+        : pos.piece_at(move.dst_sq()).get_type();
+    const auto scaled_bonus = bonus - capthist_score(hist, move) * std::abs(bonus) / 32768;
+    (*capt_hist)[pos.piece_to(move)][static_cast<int>(captured_type) - 1] += scaled_bonus;
 }
