28160288f7c2977ab082d4ed532f0655ec0ed73c
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2025-04-09 00:32:10 +0100

    Add continuation corrhist
    Bench: 3949134

--- src/history.hpp
+++ src/history.hpp
@@ -22,6 +22,7 @@ class HistoryTable {
         std::unique_ptr<MDArray<Score, 16384, 2>> pawn_corr_hist;
         std::unique_ptr<MDArray<Score, 16384, 2>> white_non_pawn_corr_hist;
         std::unique_ptr<MDArray<Score, 16384, 2>> black_non_pawn_corr_hist;
+        std::unique_ptr<MDArray<Score, 1024, 1024>> cont_corr_hist;
 
         static size_t calc_hist_idx(Move move, Side stm) { return move.hist_idx(stm); };
         static HistoryValue bonus(int depth) { return std::min(16 * (depth + 1) * (depth + 1), 1200); };
@@ -34,6 +35,7 @@ class HistoryTable {
             pawn_corr_hist = std::make_unique<MDArray<Score, 16384, 2>>();
             white_non_pawn_corr_hist = std::make_unique<MDArray<Score, 16384, 2>>();
             black_non_pawn_corr_hist = std::make_unique<MDArray<Score, 16384, 2>>();
+            cont_corr_hist = std::make_unique<MDArray<Score, 1024, 1024>>();
             clear(); 
         };
 
@@ -41,19 +43,20 @@ class HistoryTable {
         HistoryValue mainhist_score(Move move, Side stm) const { return main_hist[move.hist_idx(stm)]; };
         HistoryValue conthist_score(const BoardHistory& hist, Move move) const;
         HistoryValue capthist_score(const BoardHistory& hist, const Move move) const;
-        Score corrhist_score(const Position& pos, const Score static_eval) const;
+        Score corrhist_score(const Position& pos, const Score static_eval, const BoardHistory& hist) const;
 
         void update_scores(const BoardHistory& hist, std::span<const Move> moves, ScoredMove current_move, Side stm, int depth);
         void update_mainhist_score(Move move, Side stm, HistoryValue bonus);
         void update_conthist_score(const BoardHistory& hist, Move move, HistoryValue bonus);
         void update_capthist_score(const BoardHistory& hist, Move move, HistoryValue bonus);
-        void update_corrhist_score(const Position& pos, const Score static_eval, const Score search_score, const int depth);
+        void update_corrhist_score(const Position& pos, const Score static_eval, const Score search_score, const int depth, const BoardHistory& hist);
         void clear() { 
             std::for_each(cont_hist->begin(), cont_hist->end(), [](auto& arr) { arr.fill(0); });
             std::for_each(capt_hist->begin(), capt_hist->end(), [](auto& arr) { arr.fill(0); });
             std::for_each(pawn_corr_hist->begin(), pawn_corr_hist->end(), [](auto& arr) { arr.fill(0); });
             std::for_each(white_non_pawn_corr_hist->begin(), white_non_pawn_corr_hist->end(), [](auto& arr) { arr.fill(0); });
             std::for_each(black_non_pawn_corr_hist->begin(), black_non_pawn_corr_hist->end(), [](auto& arr) { arr.fill(0); });
+            std::for_each(cont_corr_hist->begin(), cont_corr_hist->end(), [](auto& arr) { arr.fill(0); });
             main_hist.fill(0); 
         };
 };
