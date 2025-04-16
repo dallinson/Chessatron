e5cbc0445a829493059213719d16aa1ec2d5886b
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2025-04-16 01:27:13 +0100

    Change history pruning threshold for quiet moves
    Bench: 4216896

--- src/search.cpp
+++ src/search.cpp
@@ -402,7 +402,7 @@ Score SearchHandler::negamax_step(const Position& old_pos, Score alpha, Score be
         // history pruning
         if constexpr (!is_pv_node(node_type)) {
             if (best_score > (MagicNumbers::NegativeInfinity + MAX_PLY) && evaluated_moves.size() > 0 && depth <= hp_depth && static_eval <= alpha
-                && hist_score < -(depth * depth) * hp_multi) {
+                && hist_score < (move.move.is_noisy() ? -(depth * depth) : -depth) * hp_multi) {
                 continue;
             }
         }
