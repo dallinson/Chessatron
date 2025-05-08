986ae9f7a1ff6e7fcfc824b6802a6c8a18e85d7a
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2025-05-08 07:20:40 +0100

    Remove unused function in move ordering (#146)
    Bench: 4617376

--- src/move_ordering.hpp
+++ src/move_ordering.hpp
@@ -14,6 +14,4 @@ class MovePicker {
     public:
         MovePicker(MoveList&& input_moves, const Position& pos, const BoardHistory& hist, const Move pv_move, const HistoryTable& history_table, Move killer);
         std::optional<ScoredMove> next(const bool skip_quiets);
-
-        const ScoredMove& operator[](size_t idx) { return moves[idx]; };
 };
