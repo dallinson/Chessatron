1c51146bb4fb9708e1650f4fe300aa79cb336e5a
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2025-12-02 11:40:54 +0000

    Check new killer move code doesn't lose elo
    Bench: 4360783

--- src/search.hpp
+++ src/search.hpp
@@ -24,7 +24,7 @@ constexpr auto default_see_rook_value = 474;
 constexpr auto default_see_queen_value = 933;
 
 constexpr auto LMR_QUANT_CONSTANT = 1024;
-constexpr auto KILLER_MOVE_COUNT = 2;
+constexpr auto KILLER_MOVE_COUNT = 1;
 
 enum class NodeTypes {
     ROOT_NODE,
