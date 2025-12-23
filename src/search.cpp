86a6a060252eb11ee7014d12bdb31f33840870d3
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2025-12-23 22:02:46 +0000

    Skip quiets early if found move that avoids checkmate
    Bench: 8135262

--- src/search.cpp
+++ src/search.cpp
@@ -600,7 +600,7 @@ Score SearchHandler::quiescent_search(const Position& old_pos, Score alpha, Scor
     int total_moves = 0;
     Move best_move = Move::NULL_MOVE();
     std::optional<ScoredMove> opt_move;
-    while ((opt_move = mp.next(false)).has_value()) {
+    while ((opt_move = mp.next(best_score <= (MagicNumbers::NegativeInfinity + MAX_PLY))).has_value()) {
         if (search_cancelled) {
             break;
         }
