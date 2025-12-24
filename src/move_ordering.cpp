83868a0ce6a267f090281f16cb77c9192807b94d
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2025-12-24 14:18:23 +0000

    Use Stormphrax noisy move scoring
    Bench: 8918406

--- src/move_ordering.cpp
+++ src/move_ordering.cpp
@@ -14,7 +14,11 @@ auto MovePicker::score_noisies() -> void {
                                     ? PieceTypes::PAWN
                                     : pos.piece_at(move.move.dst_sq()).type();
         const auto dest_score = Search::SEEScores[static_cast<u8>(dest_type)];
-        move.score = ((100000 * dest_score) + hist_table.capthist_score(board_hist, move.move));
+        move.score = hist_table.capthist_score(board_hist, move.move) / 8;
+        move.score += dest_score;
+        if (move.move.is_promotion()) {
+            move.score += (Search::SEEScores[static_cast<u8>(move.move.promo_type())] - Search::SEEScores[static_cast<u8>(PieceTypes::PAWN)]);
+        }
     }
 }
 
