98f6d6a3eb20caa6bba3f3b7c8e4b7ebe1e26531
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2026-04-06 16:45:43 +0100

    Do not return early if only a single move possible
    Bench: 7592218

--- src/search.cpp
+++ src/search.cpp
@@ -688,15 +688,6 @@ Move SearchHandler::run_iterative_deepening_search() {
     pv_move = Move::NULL_MOVE();
     // reset pv move so we don't accidentally play an illegal one from a previous search
     const auto search_start_point = std::chrono::steady_clock::now();
-    // TranspositionTable transpositions;
-    auto moves =
-        MoveGenerator::generate_legal_moves<MoveGenType::ALL_LEGAL>(board_hist[board_hist.len() - 1], board_hist[board_hist.len() - 1].stm());
-    // We generate legal moves only as it saves us having to continually rerun legality checks
-    if (moves.size() == 1) {
-        return moves[0].move;
-        // If only one move is legal in this position we don't need to search; we can just return the one legal move
-        // in order to save some time
-    }
 
     node_spent_table.fill(0);
     pv_table.pv_length.fill(0);
