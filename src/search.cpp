47be4be22714509fe37d843d5cfa3d08f33cfd0b
Daniel Allinson <56653697+dallinson@users.noreply.github.com>
2026-07-21 19:49:15 +0100

    Check for draw in qsearch
    Bench: 8215780

--- src/search.cpp
+++ src/search.cpp
@@ -539,6 +539,10 @@ Score SearchHandler::negamax_step(const Position& old_pos, Score alpha, Score be
 template <NodeTypes node_type>
 Score SearchHandler::quiescent_search(const Position& old_pos, Score alpha, Score beta, int ply, uint64_t& node_count) {
 
+    if (Search::is_draw(old_pos, board_hist)) {
+        return 0;
+    }
+
     const auto entry = tt.probe(old_pos);
     const auto tt_hit = entry.has_value();
     if constexpr (!is_pv_node(node_type)) {
